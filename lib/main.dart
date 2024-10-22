812040dc044626f522e0a71696e8bd8a7a425a24
Gabriela Cristina Becker <gabriela.becker@angellira.com.br>
2024-10-22 15:09:19 -0300

    Aula 31

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 
 import 'package:flutter/material.dart';
-main(){
-  runApp(new PerguntaApp());
-}
+main() => runApp(new PerguntaApp()); //arrow function
 
+//StatelessWidget - basicamente um componente sem estado
 class PerguntaApp extends StatelessWidget {
 
+  @override //obrigatoriamente o stateless precisa implementar;
   Widget build(BuildContext context) {
+    //widget MaterialApp
     return MaterialApp(
-      home: Text('Olá Flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+        ),
+        body: Text('Olá Flutter!!!'),
+      ), //Estruturar a aplicação 
     );
   }
 }
