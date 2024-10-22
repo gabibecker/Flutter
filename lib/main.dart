7c1d33af0f969466ff09424d6a03b6f60378763a
Gabriela Cristina Becker <gabriela.becker@angellira.com.br>
2024-10-22 15:32:12 -0300

    Aula 35

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ main() => runApp(new PerguntaApp()); //arrow function
 
 //StatelessWidget - basicamente um componente sem estado
 class PerguntaApp extends StatelessWidget {
+  final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?',
+  ];
 
   @override //obrigatoriamente o stateless precisa implementar;
   Widget build(BuildContext context) {
@@ -13,7 +17,25 @@ class PerguntaApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          //criando uma coluna para poder agrupar widgets
+          children: <Widget> [
+            Text(perguntas[0]),
+            //botao
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+          ],
+        ),
       ), //Estruturar a aplicação 
     );
   }
